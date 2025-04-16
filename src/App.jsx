bdd6352508eb849aff288696a6df329ad2742c90
Tania Dumych <tanja11.malvchko@gmail.com>
2025-04-16 23:45:33 +0200

    commit three

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@
 import './App.css'
 import userData from './userData.json';
 import Profile from './components/Profile/Profile';
+import friends from './friends.json';
+import FriendList from './components/Profile/FriendList/FriendList';
+import transactions from './transactions.json';
+import TransactionHistory from './components/Profile/TransactionHistory/TransactionHistory';
+
 
 export default function App() {
   return (
@@ -13,6 +18,10 @@ export default function App() {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
+       <TransactionHistory items={transactions} />
     </>
+    
   );
+ 
 }
---
+++ src/components/Profile/FriendList/FriendList.jsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx';
+import FriendListItem from './FriendListItem';
+import css from './FriendList.module.css';
+
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li key={id} className={css.item}>
+          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
